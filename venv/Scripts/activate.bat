5e2a7145604b596cd80a7ad6b388973590105738
Yuvaraj S R <yuvarajsrty@gmail.com>
2024-01-30 15:45:49 +0530

    Reworked
    Signed-off-by: Yuvaraj S R <yuvarajsrty@gmail.com>

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Own Projects\Flask_Python\venv
+set VIRTUAL_ENV=D:\Own Projects\Flask_Python_Final\Flask_Speech_Transcription\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
